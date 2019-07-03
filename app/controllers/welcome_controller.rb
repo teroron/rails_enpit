f2e476ea63c5379752ee147bdca27976ce90e61b
ryusei <a1908rk@aiit.ac.jp>
2019-07-03 10:19:19 +0000

    controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+   def index
+   
+       @user = Hash.new
+       @user[:name] = 'teroron'
+       @user[:username] = 'teroron10'
+       @user[:location] = 'Tokyo'
+       @user[:about] = 'Hello.'
+
 end
